4cd28e6575bde48e614ab071b05d0372580b3b91
manojkumar1053 <manojkumar1053@gmail.com>
2021-10-24 21:20:06 -0400

    stacking decorator)

--- decorator_2.py
+++ decorator_2.py
@@ -1,1 +1,27 @@
-def
+def add_four(func):
+    def wrapper_func(*args, **kwargs):
+        print("Adding 4")
+        result = func(*args, **kwargs)
+        return result + 4
+
+    return wrapper_func
+
+
+def double(func):
+    def wrapper_func(*args, **kwargs):
+        print("Double the result ")
+        result = func(*args, **kwargs)
+        return result * 2
+
+    return wrapper_func()
+
+
+@double
+@add_four
+def add(a, b):
+    print(f"we are adding {a} and {b}")
+    return a + b
+
+
+print(add(1, 3))
+print(add(3, 5))
