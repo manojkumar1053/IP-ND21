f6b987c4353b1c964016ff5393d42559defac241
manojkumar1053 <manojkumar1053@gmail.com>
2021-10-31 23:38:25 -0400

    adding new method to create word frequency table in python

--- count.py
+++ count.py
@@ -5,12 +5,18 @@ def count_unique_words(filename):
         content = f.read()
         words = content.split()
 
-        for word in words:
-            if word in char_freq:
-                char_freq[word] += 1
-            else:
-                char_freq[word] = 1
+        # Method 1 to create the word freq list
+        # for word in words:
+        #     if word in char_freq:
+        #         char_freq[word] += 1
+        #     else:
+        #         char_freq[word] = 1
 
+        # Method 2 craete the word freq list
+        for word in words:
+            char_freq[word] = char_freq.get(word, 0) + 1
+
+    # Sort the dict baased on the value and then get the top 10 values
     top_words_list = sorted(char_freq.items(), key=lambda x: x[1], reverse=True)[:10]
     for tup in top_words_list:
         word, freq = tup
