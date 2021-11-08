afe8d145a753d0a27b6980d6f86683762fb01d71
manojkumar1053 <manojkumar1053@gmail.com>
2021-11-08 08:46:18 -0500

    test2

---
+++ conext-manager2.py
@@ -0,0 +1,21 @@
+from contextlib import contextmanager
+
+
+@contextmanager
+def my_context(val=42):
+    try:
+        print("Initialing Context")
+        yield val
+    except BaseException as err:
+        print(f"CM body raised {err.__class__.__name__}({err.args[0]})")
+        print(f"Inspect {err.__traceback__}")
+    finally:
+        print("Exiting context")
+
+
+with my_context() as t:
+    print("The answer is", t)
+
+with my_context(33) as t:
+    x = t / 0
+    print(x)
