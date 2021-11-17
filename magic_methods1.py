f5b8fe87a3b07db0ca2c15d4986af46c23ac1957
manojkumar1053 <manojkumar1053@gmail.com>
2021-11-17 07:32:52 -0500

    private set

--- magic_methods1.py
+++ magic_methods1.py
@@ -8,12 +8,17 @@ class MagicShoppingCart:
     def __str__(self):
         return f"MagicShoppingCart({self.items})"
 
+    def __contains__(self, item):
+        return item.lower() in self.items
+
 
 cart = MagicShoppingCart({
-    "Apple": 20,
-    "bananas": 27,
-    "oranges": 17
+    "Apples".lower(): 20,
+    "bananas".lower(): 27,
+    "oranges".lower(): 17
 })
 
 print(len(cart))
 print(cart)
+print("Apples" in cart)
+print("grapes" in cart)
