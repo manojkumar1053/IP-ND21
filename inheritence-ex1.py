f363b2d9a2b676d3b36b68e79f290f1428a2a6ff
manojkumar1053 <manojkumar1053@gmail.com>
2021-11-18 10:52:28 -0500

    test

---
+++ inheritence-ex1.py
@@ -0,0 +1,34 @@
+class MotarVehicle:
+    def __init__(self, range):
+        self.range = range
+        self.tank = range
+
+    def travel(self, distance):
+        if distance > self.tank:
+            print("Not Enough")
+            self.tank = 0
+        else:
+            print(f"VOOM ! travelled {distance} kms !!")
+
+    def refuel(self):
+        print("Refueling....")
+        self.tank = self.range
+
+    def __str__(self):
+        return f" Vehicle(range= {self.range}, tank={self.tank})"
+
+
+class Car(MotarVehicle):
+    def __init__(self, range, wheels, color):
+        super().__init__(range)
+        self.wheels = wheels
+        self.color = color
+
+
+mv = MotarVehicle(999)
+print(mv)
+mv.travel(400)
+print(mv)
+
+c = Car(500, 4, 'Blue')
+print(c.range, c.tank, c.wheels, c.color)
