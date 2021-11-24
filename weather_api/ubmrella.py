4bab1147c74a686c4f5494223bd90d22b28375ec
manojkumar1053 <manojkumar1053@gmail.com>
2021-11-24 09:13:11 -0500

    weather api

---
+++ weather_api/ubmrella.py
@@ -0,0 +1,8 @@
+class WeatherService:
+
+    @classmethod
+    def getForecast(cls, city, country):
+        pass
+
+if __name__ == "__main__":
+    print(WeatherService.getForecast())
---
+++ weather_api/weatherService.py
@@ -0,0 +1,24 @@
+import requests
+
+
+class WeatherService:
+    baseUrl = 'https://api.openweathermap.org/data/2.5'
+    appId = ''
+
+    @classmethod
+    def getForecast(cls, city="new york", country="us"):
+        url = f'{cls.baseUrl}/forecast'
+
+        response = requests.get(url, params=[
+            ('q', f'{city},{country}'),
+            ('mode', 'json'),
+            ('APPID', cls.appId)
+        ])
+
+        data = response.json()
+
+        return data['list']
+
+
+if __name__ == "__main__":
+    print(WeatherService.getForecast())
