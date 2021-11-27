de9ced6b6b62ddb7e05caff9efde3404327f0fd5
manojkumar1053 <manojkumar1053@gmail.com>
2021-11-26 21:14:49 -0500

    test flask end points

--- flask-test/app.py
+++ flask-test/app.py
@@ -2,12 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return 'helloworld'
+    return 'yo'
 
+
+@app.route('/')
+def candy():
+    return 'chocolate'
+
+
+@app.route('/')
+def greeting():
+    return 'hello!'
 
 # Running Test Server on Window CMD
 # export FLASK_APP=app.py
 # flask run --host 0.0.0.0 --port 3000 --reload
-
